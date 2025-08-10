e5afdc01b4f3d7367582744936fe7fc6aee194dc
ammiexx <animutalemneh632@gmail.com>
2025-08-10 14:52:56 +0300

    add changes

--- painting/settings.py
+++ painting/settings.py
@@ -75,11 +75,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'painting.wsgi.application'
 import dj_database_url
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': dj_database_url.parse(config("DATABASE_URL"))
 }
 
 AUTH_PASSWORD_VALIDATORS = [
