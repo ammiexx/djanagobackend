7aab7d50b16479c9c9648b0efeec213237ebf9d3
ammiexx <animutalemneh632@gmail.com>
2025-08-20 15:28:05 +0300

    add changes

--- painting/settings.py
+++ painting/settings.py
@@ -34,6 +34,17 @@ REST_FRAMEWORK = {
     ],
 }
 
+
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME')  # Optional
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -65,6 +76,7 @@ INSTALLED_APPS = [
     'newproducts',
     'upcomming',
     'message',
+    'storages',
 
 ]
 ASGI_APPLICATION = "your_project_name.asgi.application"
