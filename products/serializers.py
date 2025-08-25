c0c345899e560665f173d723d7eec88321715839
ammiexx <animutalemneh632@gmail.com>
2025-08-25 14:46:28 +0300

    add changes

--- products/serializers.py
+++ products/serializers.py
@@ -15,31 +15,25 @@ class ProductImageSerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     images = ProductImageSerializer(many=True, read_only=True)
 
+    # Writable fields
+    product_photo = serializers.ImageField(required=False, allow_null=True)
+    profile_photo = serializers.ImageField(required=False, allow_null=True)
+
+    # Additional image uploads
     uploaded_images = serializers.ListField(
         child=serializers.ImageField(max_length=1000000, allow_empty_file=False, use_url=False),
         write_only=True,
         required=False
     )
 
-    product_photo = serializers.SerializerMethodField()
-    profile_photo = serializers.SerializerMethodField()
-
-    def get_product_photo(self, obj):
-        return obj.product_photo.url if obj.product_photo else None
-
-    def get_profile_photo(self, obj):
-        return obj.profile_photo.url if obj.profile_photo else None
-
     class Meta:
         model = Product
-        fields = '__all__'  # all fields from model
-        extra_fields = ['images', 'uploaded_images', 'product_photo', 'profile_photo']
+        fields = '__all__'
 
     def create(self, validated_data):
         uploaded_images = validated_data.pop('uploaded_images', [])
         product = Product.objects.create(**validated_data)
 
-        # Save related images (limit to 10)
         for i, image in enumerate(uploaded_images):
             if i >= 10:
                 break
