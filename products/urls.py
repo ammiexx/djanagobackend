8a97877587463cbd52e1d65725b158cd3481b97e
ammiexx <animutalemneh632@gmail.com>
2025-08-26 09:12:54 +0300

    add changes

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProductListCreateAPIView
+from .views import ProductListCreateAPIView,MyProductListAPIView
 
 urlpatterns = [
     path('products/', ProductListCreateAPIView.as_view(), name='product-list-create'),
+    path('myproducts/',MyProductListAPIView.as_view(),name='my-product-list'),
 ]
--- products/views.py
+++ products/views.py
@@ -5,3 +5,11 @@ from .serializers import ProductSerializer
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+class MyProductListAPIView(generics.ListAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        user_id = self.request.query_params.get('userId')
+        if user_id:
+            return Product.objects.filter(clerk_user_id=user_id)
+        return Product.objects.none()
