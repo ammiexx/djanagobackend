b959765768a21aaf7a48e2e4a406285dd6c7a6e0
ammiexx <animutalemneh632@gmail.com>
2025-09-06 12:58:13 +0300

    add changes

--- products/admin.py
+++ products/admin.py
@@ -9,15 +9,3 @@ class ProductImageInline(admin.TabularInline):
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     inlines = [ProductImageInline]
-    list_display = ("product_name", "company_name", "category", "verified", "date_posted")
-    list_filter = ("verified", "category")
-    search_fields = ("product_name", "company_name")
-    actions = ["mark_verified", "mark_unverified"]
-
-    def mark_verified(self, request, queryset):
-        queryset.update(verified=True)
-    mark_verified.short_description = "Mark selected products as Verified"
-
-    def mark_unverified(self, request, queryset):
-        queryset.update(verified=False)
-    mark_unverified.short_description = "Mark selected products as Unverified"
