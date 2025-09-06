8f7e200d7b98315836e0ec2931e494064fae1e8b
ammiexx <animutalemneh632@gmail.com>
2025-09-06 11:49:07 +0300

    add  changes

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProductListCreateAPIView,MyProductListAPIView,ProductRetrieveUpdateDestroyAPIView,NearbyProductListAPIView
+from .views import ProductListCreateAPIView,MyProductListAPIView,ProductRetrieveUpdateDestroyAPIView,NearbyProductListAPIView,PrductListCreateAPIView
 
 urlpatterns = [
     path('products/', ProductListCreateAPIView.as_view(), name='product-list-create'),
+    path('product/', PrductListCreateAPIView.as_view(), name='product-list-create'),
     path('myproducts/',MyProductListAPIView.as_view(),name='my-product-list'),
     path('delete/products/<int:pk>/', ProductRetrieveUpdateDestroyAPIView.as_view(), name='product-detail'),  # ✅ For GET, PATCH, DELETE
     path("products/nearby/", NearbyProductListAPIView.as_view(), name="nearby-products"),
