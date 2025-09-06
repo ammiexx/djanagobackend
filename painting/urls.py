c727ca18bb934e1e00484b12b0153e83673f8d07
ammiexx <animutalemneh632@gmail.com>
2025-09-06 12:04:03 +0300

    add changes

--- painting/urls.py
+++ painting/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path('api/', include('notification.urls')),
     path('api/',include('chat.urls')),
     path('api/', include('products.urls')),
-    path('api/',include('products.urls')),
     path('api/',include('realestate.urls')),
     path('backend/',include('electronic.urls')),
     path('backend/',include('car.urls')),
