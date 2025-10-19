a07d9c6dce5b3858446dd4173d2d84cbc3acd6b4
ammiexx <animutalemneh632@gmail.com>
2025-10-19 13:04:17 +0300

    add the changes of models.py and migrations file of products app

--- products/models.py
+++ products/models.py
@@ -12,6 +12,11 @@ class Product(models.Model):
     product_photo = CloudinaryField('image',null=True,blank=True)
     description = models.TextField(blank=True)
     product_video = CloudinaryField('video',resource_type='video', null=True, blank=True)
+    discount_start_date = models.PositiveIntegerField(
+    null=True,
+    blank=True,
+    help_text="Number of days this discount will stay active"
+)
     discount_duration = models.PositiveIntegerField(
     null=True,
     blank=True,
