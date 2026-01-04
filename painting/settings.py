5d8b3aef498faf6737ae69895aae79738ec2dfda
biruk11011@gmail.com <biruknigusie00@gmail.com>
2026-01-04 13:13:04 +0300

    Add Setting.py changes

--- painting/settings.py
+++ painting/settings.py
@@ -132,6 +132,9 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
